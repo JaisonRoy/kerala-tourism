fd1d5416b2fdb20a6e4fc871f9293554b446009a
JaisonRoy <jaisonroy700@gmail.com>
2021-11-15 18:20:42 +0530

    7

--- JS/login.js
+++ JS/login.js
@@ -3,40 +3,61 @@ let pass = document.getElementById("pass")
 let emailerror = document.getElementById("emailerror")
 let passerror = document.getElementById("passerror")
 
-let emailreg = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/
+let emailreg = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9]+)\.([a-z]{2,3})$/;
 let valpass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/
 
 function formvalidate() {
-           console.log(email.value)
-           console.log((emailreg.test(email.value)));
-           console.log((valpass.test(pass.value)));
+    // console.log(email.value)
+    // console.log((emailreg.test(email.value)));
+    // console.log((valpass.test(pass.value)));
 
-if(emailreg.test(email.value.trim()) == true && valpass.test(pass.value.trim()) == true){
-        emailerror.innerHTML="valid";
-        email.style.border="2px solid green";
-        passerror.innerHTML="valid";
-        pass.style.border="2px solid green";
+    if(email.value=="" && pass.value==""){
+        emailerror.innerHTML="";
+        passerror.innerHTML="";
+        return false;
+    }
+
+
+    else if (emailreg.test(email.value) == true && valpass.test(pass.value) == true) {
+        emailerror.innerHTML = "&#10003;";
+        emailerror.style.color = "green"
+        email.style.border = "2px solid green";
+        passerror.innerHTML = "&#10003;";
+        passerror.style.color = "green"
+        pass.style.border = "2px solid green";
+
         return true;
-}else if(emailreg.test(email.value.trim()) == false && valpass.test(pass.value.trim()) == false ){
-        emailerror.innerHTML = "invalid";
-        email.style.border="2px solid red";
-        passerror.innerHTML="invalid password";
-        pass.style.border="2px solid red";
-        return false
-}else if(emailreg.test(email.value.trim()) == true && valpass.test(pass.value.trim()) == false){
-     emailerror.innerHTML="valid";
-     email.style.border="2px solid green";
-     passerror.innerHTML="invalid password";
-     pass.style.border="2px solid red";
-     return false;
-}else{
-    emailerror.innerHTML = "invalid";
-    email.style.border="2px solid red";
-    passerror.innerHTML="valid";
-    pass.style.border="2px solid green";
-    return false;
-}
+    } else if (emailreg.test(email.value) == false && valpass.test(pass.value) == false) {
+        emailerror.innerHTML = "invalid email";
+        emailerror.style.color = "red"
+        email.style.border = "2px solid red";
+        passerror.innerHTML = "invalid password";
+        passerror.style.color = "red"
+        pass.style.border = "2px solid red";
 
+        return false
+    } else if (emailreg.test(email.value) == true && valpass.test(pass.value) == false) {
+        emailerror.innerHTML = "&#10003;";
+        emailerror.style.color = "green"
+        email.style.border = "2px solid green";
+        passerror.innerHTML = "invalid password";
+        passerror.style.color = "red"
+        pass.style.border = "2px solid red";
+
+        return false;
+    } else {
+        emailerror.innerHTML = "invalid email";
+        emailerror.style.color = "red"
+        email.style.border = "2px solid red";
+        passerror.innerHTML = "&#10003;";
+        passerror.style.color = "green"
+        pass.style.border = "2px solid green";
+
+        return false;
+    }
+
+
+    
 
     // if(emailreg.test(email.value)){
     //     emailerror.innerHTML="valid";
@@ -57,7 +78,7 @@ if(emailreg.test(email.value.trim()) == true && valpass.test(pass.value.trim()) 
     // }else{
     //     console.log(pass.value) 
     // }
- 
+
     // console.log(pass.value)
     // if(valpass.test(pass.value) == true ) {
     //  passerror.innerHTML="valid";
--- JS/signin.js
+++ JS/signin.js
@@ -1,3 +1,7 @@
+let fname = document.getElementById("fname")
+let lname = document.getElementById("lname")
+let adds = document.getElementById("adds")
+
 let email = document.getElementById("email")
 let phnum = document.getElementById("phno")
 let pass = document.getElementById("pass")
@@ -6,33 +10,195 @@ let emailerror = document.getElementById("emailerror")
 let numerror = document.getElementById("numerror")
 let passerror = document.getElementById("passerror")
 let confpasserror = document.getElementById("confpasserror")
+let msg = document.getElementById("msg")
 
-function validate(){
-    let emailreg =/^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9]+)\.([a-z]{2,3})(.[a-zA-Z]{2,3}?)$/;
-    let numreg =/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    let valpass =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+let emailreg = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9]+)\.([a-z]{2,3})$/;
+let numreg = /^\(?([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+let valpass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+
+function validate() {
+
+    console.log((emailreg.test(email.value)));
+    console.log((numreg.test(phnum.value)));
+    console.log((valpass.test(pass.value)));
+    console.log((fname.value));
+    console.log((lname.value));
+
+    if(fname.value.trim() ==""){
+        fname.style.border="red"
+    }
+    else if(lname.value.trim() == ""){
+        lname.style.border="red"
+    }
+    else if(adds.value.trim() == ""){
+        adds.style.border="red"
+    }
+    else{
+        fname.style.border="green"
+        lname.style.border="green"
+        adds.style.border="green"
+    }
+    if(confpass.value ==""){
+        confpasserror.innerHTML = "";
+    }
+    else if ((pass.value) !== (confpass.value)){
+        confpasserror.innerHTML = "incorrect password";
+        confpasserror.style.color ="red";
+        confpass.style.border = "2px solid red";
     
-            if(emailreg.test(email.value)){
-               emailerror.innerHTML="valid";
-               email.style.border="2px solid green";
-               return true;
-               
-           }
-            if(numreg.test(phnum.value)){
-                passerror.innerHTML="valid";
-                passerror.style.border="2px solid green";
-                return true;
-            }
-            if(valpass.test(pass.value)){
-                passerror.innerHTML="valid";
-                pass.style.border="2px solid green";
-                return true;
-            }
-            else{
-            passerror.innerHTML="invalid password";
-            passerror.style.border="2px solid red";
-            return false;
-           }
+    }
+    else{
+  
+        confpasserror.innerHTML = "&#10003";
+        confpasserror.style.color="green";
+        confpass.style.border = "2px solid green";
+    }
 
 
-}    
+
+    if (emailreg.test(email.value) == true && numreg.test(phnum.value) == true && valpass.test(pass.value) == true) {
+        emailerror.innerHTML = "&#10003;";
+        emailerror.style.color = "green";
+        email.style.border = "2px solid green";
+
+        numerror.innerHTML = "&#10003;";
+        numerror.style.color = "green";
+        phnum.style.border = "2px solid green";
+
+        passerror.innerHTML = "&#10003;";
+        passerror.style.color = "green";
+        pass.style.border = "2px solid green";
+
+        return true;
+
+    } else if (emailreg.test(email.value) == true && numreg.test(phnum.value) == true && valpass.test(pass.value) == false) {
+        emailerror.innerHTML = "&#10003;";
+        emailerror.style.color = "green";
+        email.style.border = "2px solid green";
+
+        numerror.innerHTML = "&#10003;";
+        numerror.style.color = "green";
+        phnum.style.border = "2px solid green";
+
+        passerror.innerHTML = "invalid password";
+        passerror.style.color = "red";
+        pass.style.border = "2px solid red";
+
+        return false;
+    } else if (emailreg.test(email.value) == true && numreg.test(phnum.value) == false && valpass.test(pass.value) == false) {
+        emailerror.innerHTML = "&#10003;";
+        emailerror.style.color = "green";
+        email.style.border = "2px solid green";
+
+        numerror.innerHTML = "invalid phone number";
+        numerror.style.color = "red";
+        phnum.style.border = "2px solid red";
+
+        passerror.innerHTML = "invalid password";
+        passerror.style.color = "red";
+        pass.style.border = "2px solid red";
+
+        return false;
+    } else if (emailreg.test(email.value) == false && numreg.test(phnum.value) == false && valpass.test(pass.value) == false) {
+        emailerror.innerHTML = "invalid email";
+        emailerror.style.color = "red";
+        email.style.border = "2px solid red";
+
+        numerror.innerHTML = "invalid phone number";
+        numerror.style.color = "red";
+        phnum.style.border = "2px solid red";
+
+        passerror.innerHTML = "invalid password";
+        passerror.style.color = "red";
+        pass.style.border = "2px solid red";
+
+        return false;
+    } else if (emailreg.test(email.value) == false && numreg.test(phnum.value) == false && valpass.test(pass.value) == true) {
+        emailerror.innerHTML = "invalid email";
+        emailerror.style.color = "red";
+        email.style.border = "2px solid red";
+
+        numerror.innerHTML = "invalid phone number";
+        numerror.style.color = "red";
+        phnum.style.border = "2px solid red";
+
+        passerror.innerHTML = "&#10003;";
+        passerror.style.color = "green";
+        pass.style.border = "2px solid green";
+
+        return false;
+    } else if(emailreg.test(email.value) == false && numreg.test(phnum.value) == true && valpass.test(pass.value) == false){
+        emailerror.innerHTML = "invalid email";
+        emailerror.style.color = "red";
+        email.style.border = "2px solid red";
+
+        numerror.innerHTML = "&#10003;";
+        numerror.style.color = "green";
+        phnum.style.border = "2px solid green";
+
+        passerror.innerHTML = "invalid password";
+        passerror.style.color = "red";
+        pass.style.border = "2px solid red";
+
+        return false;
+    }
+    else {
+        emailerror.innerHTML = "&#10003;";
+        emailerror.style.color = "green";
+        email.style.border = "2px solid green";
+
+        numerror.innerHTML = "invalid phone number";
+        numerror.style.color = "red";
+        phnum.style.border = "2px solid red";
+
+        passerror.innerHTML = "&#10003;";
+        passerror.style.color = "green";
+        pass.style.border = "2px solid green"; 
+    }
+
+
+
+
+ 
+
+
+}
+function validatePassword(pass) {
+                
+    if (pass.length === 0) {
+        document.getElementById("msg").innerHTML = "";
+        return;
+    }
+    var matchedCase = new Array();
+    matchedCase.push("[A-Z]"); 
+    matchedCase.push("[0-9]"); 
+    matchedCase.push("[a-z]"); 
+
+ 
+    var ctr = 0;
+    for (var i = 0; i < matchedCase.length; i++) {
+        if (new RegExp(matchedCase[i]).test(pass)) {
+            ctr++;
+        }
+    }
+    var color = "";
+    var strength = "";
+    switch (ctr) {
+        case 0:
+        case 1:
+            strength = "Very Weak";
+            color = "red";
+            break;
+        case 2:
+            strength = "Medium";
+            color = "orange";
+            break;
+        case 3:
+            strength = "Strong";
+            color = "green";
+            break;
+    }
+
+    document.getElementById("msg").innerHTML = strength;
+    document.getElementById("msg").style.color = color;
+}
